ed607cde57d74b77e863da2e8c187f14487e93d1
ViniTex231 <viniteixeira2828@gmail.com>
2024-10-17 17:14:31 -0300

    feat: first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import time
+import requests
+import json
+
+# Caminho para o arquivo
+file_path = r"C:\Users\tev3ca\Desktop\Jsons\installment.txt"
+
+# URL do endpoint para enviar os dados
+url = "http://10.234.242.212:8000/api/v1/installments/"
+
+def send_request(json_data):
+    try:
+        response = requests.post(url, json=json_data)
+        if response.status_code == 200 or 201:
+            print(f"Requisição para o representante {json_data['rep']} foi bem-sucedida.")
+        else:
+            print(f"Erro ao enviar para o representante {json_data['rep']}: {response.status_code} - {response.text}")
+    except Exception as e:
+        print(f"Falha ao enviar para o representante {json_data['rep']}: {e}")
+
+try:
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = file.read()
+        json_list = json.loads(data)
+except FileNotFoundError:
+    print(f"O arquivo {file_path} não foi encontrado.")
+except json.JSONDecodeError:
+    print("Erro ao decodificar o JSON. Verifique o formato do arquivo.")
+
+for data in json_list:
+    send_request(data)
+    time.sleep(1)
+
+print("Processo concluído.")
